21ab0c3dba384d0933fa4d671db469e7408aafeb
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2021-10-12 11:31:13 -0700

    first commit

---
+++ The Monarch Project/The_Monarch_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Monarch_ProjectApp.swift
+//  The Monarch Project
+//
+//  Created by Jimenez on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct The_Monarch_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
