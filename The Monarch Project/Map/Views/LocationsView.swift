9a2a4f96cbb0f739a53b62beded9b88a6aaf6402
Erick Sanchez <1758210+LinnierGames@users.noreply.github.com>
2022-05-16 18:13:12 -0700

    Fix indentation to 4 spaces

--- The Monarch Project/Map/Views/LocationsView.swift
+++ The Monarch Project/Map/Views/LocationsView.swift
@@ -10,63 +10,63 @@ import MapKit
 
 struct LocationsView: View {
 
-  @EnvironmentObject private var vm: LocationsViewModel
+    @EnvironmentObject private var vm: LocationsViewModel
 
-  var body: some View {
-    ZStack{
-      Map(coordinateRegion: $vm.mapRegion)
-        .ignoresSafeArea()
+    var body: some View {
+        ZStack{
+            Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
 
-      VStack(spacing: 0) {
-        header
-          .padding()
-          .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                    .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
 
 
-        //                Spacer()
-      }
+                //                Spacer()
+            }
 
+        }
     }
-  }
 }
 
 struct LocationsView_Previews: PreviewProvider {
-  static var previews: some View {
-    LocationsView()
-      .environmentObject(LocationsViewModel())
-  }
+    static var previews: some View {
+        LocationsView()
+            .environmentObject(LocationsViewModel())
+    }
 }
 
 extension LocationsView {
 
-  private var header: some View {
-    VStack {
-      Button {
-        vm.toggleLocationsList()
-      } label: {
-        Text("\(vm.mapLocation.name), \(vm.mapLocation.cityName)")
-          .font(.title2)
-          .fontWeight(.black)
-          .foregroundColor(.primary)
-          .frame(height: 55)
-          .frame(maxWidth: .infinity)
-          .background(Color.white)
-          .overlay(alignment: .leading) {
-            Image(systemName: "arrow.down")
-              .foregroundColor(.primary)
-              .font(.headline)
-              .padding()
-          }
-          .cornerRadius(10)
-      }
+    private var header: some View {
+        VStack {
+            Button {
+                vm.toggleLocationsList()
+            } label: {
+                Text("\(vm.mapLocation.name), \(vm.mapLocation.cityName)")
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .foregroundColor(.primary)
+                            .font(.headline)
+                            .padding()
+                    }
+                    .cornerRadius(10)
+            }
 
-      if vm.showLocationsList {
-        LocationsListView()
-          .frame(maxWidth: .infinity)
-          .background(Color.red)
-      } else {
-        Spacer()
-      }
+            if vm.showLocationsList {
+                LocationsListView()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.red)
+            } else {
+                Spacer()
+            }
+        }
     }
-  }
 }
