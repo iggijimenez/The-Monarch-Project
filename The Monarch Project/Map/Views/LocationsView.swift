e54cd8cd5784ebdaffbe4cc590a629f697da947b
Erick Sanchez <1758210+LinnierGames@users.noreply.github.com>
2022-05-16 18:11:31 -0700

    Moved "This is the list" content outside of the button label

--- The Monarch Project/Map/Views/LocationsView.swift
+++ The Monarch Project/Map/Views/LocationsView.swift
@@ -9,95 +9,64 @@ import SwiftUI
 import MapKit
 
 struct LocationsView: View {
-    
-    @EnvironmentObject private var vm: LocationsViewModel
-    
-    var body: some View {
-        ZStack{
-            Map(coordinateRegion: $vm.mapRegion)
-                .ignoresSafeArea()
-            
-            VStack(spacing: 0) {
-                header
-                    .padding()
-                    .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
-                
-                
-                //                Spacer()
-            }
-            
-        }
+
+  @EnvironmentObject private var vm: LocationsViewModel
+
+  var body: some View {
+    ZStack{
+      Map(coordinateRegion: $vm.mapRegion)
+        .ignoresSafeArea()
+
+      VStack(spacing: 0) {
+        header
+          .padding()
+          .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+
+
+        //                Spacer()
+      }
+
     }
+  }
 }
 
 struct LocationsView_Previews: PreviewProvider {
-    static var previews: some View {
-        LocationsView()
-            .environmentObject(LocationsViewModel())
-    }
+  static var previews: some View {
+    LocationsView()
+      .environmentObject(LocationsViewModel())
+  }
 }
 
 extension LocationsView {
-    
-    private var header: some View {
-        
-        VStack {
-            
-            Button {
-                vm.toggleLocationsList()
-            } label: {
-                if #available(iOS 15.0, *) {
-                    VStack {
-                        //This is the Label
-                        Text("\(vm.mapLocation.name), \(vm.mapLocation.cityName)")
-                            .font(.title2)
-                            .fontWeight(.black)
-                            .foregroundColor(.primary)
-                            .frame(height: 55)
-                            .frame(maxWidth: .infinity)
-                            .background(Color.white)
-                            .overlay(alignment: .leading) {
-                                Image(systemName: "arrow.down")
-                                    .foregroundColor(.primary)
-                                    .font(.headline)
-                                    .padding()
-                            }
-                            .cornerRadius(10)
-                        
-                        Spacer()
-                    }
-                    
-                    if !vm.showLocationsList {
-                        //                    VStack{
-                        //This is the list
-                        LocationsListView()
-                            .frame(maxWidth: .infinity)
-                            .background(Color.red)
-                        Text("inside the if")
-                        //                    }
-                    }
-                    else {
-                        Spacer()
-                    }
-                } else {
-                    // Fallback on earlier versions
-                    Text("\(vm.mapLocation.name), \(vm.mapLocation.cityName)")
-                        .font(.title2)
-                        .fontWeight(.black)
-                        .foregroundColor(.primary)
-                        .frame(height: 55)
-                        .background(Color.white)
-                        .frame(maxWidth: .infinity)
-                    Text("Else statement")
-                    ZStack{
-                        Image(systemName: "arrow.down")
-                            .foregroundColor(.primary)
-                            .font(.headline)
-                            .padding()
-                    }
-                }
-            }
-        }
-        
+
+  private var header: some View {
+    VStack {
+      Button {
+        vm.toggleLocationsList()
+      } label: {
+        Text("\(vm.mapLocation.name), \(vm.mapLocation.cityName)")
+          .font(.title2)
+          .fontWeight(.black)
+          .foregroundColor(.primary)
+          .frame(height: 55)
+          .frame(maxWidth: .infinity)
+          .background(Color.white)
+          .overlay(alignment: .leading) {
+            Image(systemName: "arrow.down")
+              .foregroundColor(.primary)
+              .font(.headline)
+              .padding()
+          }
+          .cornerRadius(10)
+      }
+
+      if vm.showLocationsList {
+        LocationsListView()
+          .frame(maxWidth: .infinity)
+          .background(Color.red)
+      } else {
+        Spacer()
+      }
     }
+  }
 }
