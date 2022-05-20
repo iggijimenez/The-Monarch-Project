c591280e4ff449ff3c5f42a036244ca1305451d5
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2022-05-19 21:30:56 -0700

    added map pins to each location, can also be tapped

--- The Monarch Project/Map/Views/LocationsView.swift
+++ The Monarch Project/Map/Views/LocationsView.swift
@@ -14,7 +14,7 @@ struct LocationsView: View {
     
     var body: some View {
         ZStack{
-            Map(coordinateRegion: $vm.mapRegion)
+            mapLayer
                 .ignoresSafeArea()
             
             VStack(spacing: 0) {
@@ -22,18 +22,8 @@ struct LocationsView: View {
                     .padding()
                     .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
                 
-                ZStack{
-                    ForEach(vm.locations) { location in
-                        if vm.mapLocation == location {
-                            LocationPreviewView(location: location)
-                                .shadow(color: Color.black.opacity(0.3), radius: 20)
-                                .padding()
-                                .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
-                        }
-                    }
-                }
+                locationsPreviewStack
             }
-            
         }
     }
 }
@@ -73,12 +63,43 @@ extension LocationsView {
             
             if vm.showLocationsList {
                 LocationsListView()
-//                    .frame(maxWidth: .infinity)
+                //                    .frame(maxWidth: .infinity)
                     .background(.thinMaterial)
                     .cornerRadius(10)
                     .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
             }
         }
         
+        
+        
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.locations,
+            annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        })
+        
+    }
+    
+    private var locationsPreviewStack: some View {
+        ZStack{
+            ForEach(vm.locations) { location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(color: Color.black.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+                }
+            }
+        }
     }
 }
